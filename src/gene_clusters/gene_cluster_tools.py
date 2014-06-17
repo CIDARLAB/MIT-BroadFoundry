15f3342540e1eae7824c9dd6adb18d1e22f9a54b
Thomas Gorochowski <Tom@Eve-MkII.local>
2014-06-17 08:58:45 -0400

    GeneCluster now GeneClusterLibrary

--- src/gene_clusters/gene_cluster_tools.py
+++ src/gene_clusters/gene_cluster_tools.py
@@ -27,7 +27,7 @@ __author__  = 'Thomas E. Gorochowski <tom@chofski.co.uk>, Voigt Lab, MIT'
 __license__ = 'OSI Non-Profit OSL 3.0'
 __version__ = '1.0'
 
-class GeneCluster:
+class GeneClusterLibrary:
 	"""Class encapsulating the concept of a gene cluster and potential variants.
 	"""
 

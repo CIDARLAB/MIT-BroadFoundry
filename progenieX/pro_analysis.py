ebdc0da15b56eabcb6d8489a52cfc1659ce7dba7
yoeric <ericyoung7@gmail.com>
2014-06-17 21:54:40 -0400

    Create pro_analysis.py

---
+++ progenieX/pro_analysis.py
@@ -0,0 +1,23 @@
+from per_stepwindow import per_stepwindow
+from tata_stepwindow import tata_stepwindow
+from tfbs_finder import tfbs_finder
+from tss_finder import tss_finder
+
+def maine():
+    promoter_analysis(raw_input('Prefix:'))
+    
+def promoter_analysis(prefix) :
+
+    input_file = '%(prefix)sgen.txt' % {'prefix': prefix}
+    nucpct_file = '%(prefix)s_stepdata.txt' % {'prefix': prefix}
+    tata_file = '%(prefix)s_TATAdata.txt' % {'prefix': prefix}
+    tfbs_file = '%(prefix)s_tfbsdata.txt' % {'prefix': prefix}
+    tss_file = '%(prefix)s_tssdata.txt' % {'prefix': prefix}
+
+    per_stepwindow(input_file, nucpct_file)
+    tata_stepwindow(input_file, tata_file)
+    tfbs_finder(input_file, tfbs_file)
+    tss_finder(input_file, tss_file)
+
+if __name__ == "__maine__" :
+    maine()

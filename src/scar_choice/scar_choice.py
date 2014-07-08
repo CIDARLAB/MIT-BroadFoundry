790339a7c7fa4f0d3ba70e49e7f26e300baa453f
Thomas Gorochowski <Tom@Eve-MkII.local>
2014-07-08 13:02:37 -0400

    New script for scar choice

---
+++ src/scar_choice/scar_choice.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+"""
+Scar Choice
+===========
+
+    Script to help with the selection of scars used for Golden-Gate like
+ 	assembly of constructs.
+"""
+#    Scar Choice
+#    Copyright (C) 2014 by
+#    Thomas E. Gorochowski <tom@chofski.co.uk>
+#    All rights reserved.
+#    OSI Non-Profit Open Software License ("Non-Profit OSL") 3.0 license.
+
+import csv
+
+__author__  = 'Thomas E. Gorochowski <tom@chofski.co.uk>, Voigt Lab, MIT'
+__license__ = 'OSI Non-Profit OSL 3.0'
+__version__ = '1.0'
+
+
+
+def 
+
+
+
+def scar_compatible (scar_set, scar, max_homology=3):
+	"""Check if scar compaible with current set at given level of homology
+	"""
+
+	if 
+
+	return True
+
+
+
+
+
+
+
+
+
+
+

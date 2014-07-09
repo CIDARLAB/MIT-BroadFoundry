c1ed2d80e7fa4f3c90190af800aa26e8ccb1e924
Thomas Gorochowski <Tom@Eve-MkII.local>
2014-07-09 08:56:32 -0400

    Changes to website

--- scar_choice/scar_choice_app/app/forms.py
+++ scar_choice/scar_choice_app/app/forms.py
@@ -2,9 +2,9 @@ from flask.ext.wtf import Form
 from wtforms import TextAreaField, SubmitField, RadioField, SelectField, IntegerField
 
 class ScarChoice(Form):
-	length = SelectField(u'Scar Length: ', choices=[(2, '2 bp'), (3, '3 bp'), (4, '4 bp'), (5, '5 bp')])
-	req_num = IntegerField(u'Quantity: ')
-	homology = SelectField(u'Max Homology: ', choices=[(2, '2 bp'), (3, '3 bp'), (4, '4 bp')])
-	search_type = SelectField(u'Search Type: ', choices=[(0, 'Random'), (1, 'Enumerate')])
-	exclude_scars = TextAreaField(u'Scars to Exclude: ')
+	length = SelectField(u'Scar Length: ', choices=[(2, '2 bp'), (3, '3 bp'), (4, '4 bp'), (5, '5 bp')], default=4)
+	req_num = IntegerField(u'Quantity: ', default='1')
+	homology = SelectField(u'Max Homology: ', choices=[(1, '1 bp'), (2, '2 bp'), (3, '3 bp'), (4, '4 bp')], default=2)
+	search_type = SelectField(u'Search Type: ', choices=[(0, 'Random'), (1, 'Enumerate')], default=0)
+	exclude_scars = TextAreaField(u'Existing Scars: ')
 	submit = SubmitField(u'Generate Scars')

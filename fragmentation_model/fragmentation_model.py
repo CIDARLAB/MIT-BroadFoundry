faff28774d88be7c1aab7e93734631f670978d97
Thomas Gorochowski <tom@chofski.co.uk>
2014-07-17 22:11:30 -0400

    New fragmentation model

---
+++ fragmentation_model/fragmentation_model.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+"""
+Fragmentation Model
+===================
+
+    Implementation of a basic fragmentation model that allows for 
+    correction of read depths to account for differences in mRNA
+    length and edge effects due to sequencing being performed on 
+    fragements of a certain average size and standard deviation.
+    This is based on the approach taken by Ben Gordon but rather
+    than using a sampling approach attempts to calculate the 
+    result analytically.
+
+    Assumptions:
+
+    1. Fragmentation is a uniformly random process along the mRNA.
+          This is not necessarily valid for any fragmentation 
+          method, but it holds well enough to account for the 
+          majority of the variability. Additional baises can be
+          added at a later date.
+
+    2. Fragments are drawn from a Normal distribution defined by
+          a mean length (bp) and standard deviation (bp). This is
+          essential as it enables and exact solution to be found
+          for the predicted factor at a given base in the sequence.
+"""
+#    Fragmentation Model
+#    Copyright (C) 2014 by
+#    Thomas E. Gorochowski <tom@chofski.co.uk>
+#    All rights reserved.
+#    OSI Non-Profit Open Software License ("Non-Profit OSL") 3.0 license.
+
+__author__  = 'Thomas E. Gorochowski <tom@chofski.co.uk>, Voigt Lab, MIT'
+__license__ = 'OSI Non-Profit OSL 3.0'
+__version__ = '1.0'
+
+import math
+import numpy as np
+
+def frag_factor_profile (mrna_len, frag_mean, frag_sd):
+	"""Generate the expected fragmentation correction factor for
+	a given length mRNA and fragments of given mean length with
+	standard deviation.
+	"""
+	d = np.zeros(mrna_len)
+	for bp in range(mrna_len):
+		d[bp] = frag_at_base (bp, mrna_len, frag_mean, frag_sd)
+	return d
+
+def frag_at_base (bp, mrna_len, frag_mean, frag_sd):
+	result = 0.0
+	for i in range(1, mrna_len):
+		result += (1.0/(frag_sd*math.sqrt(2.0*frag_mean))) * math.exp(
+			       math.pow(-(i-2.0*frag_mean),2.0) / (2.0*math.pow(frag_sd,2.0))) * frag_combinations(i,bp,mrna_len)
+
+def frag_c (i, x):
+	if i <= x:
+		return 0.0
+	else:
+		return i-x
+
+def frag_combinations (i, x, x_max):
+	return i - frag_c(i,x) - frag_c(i,x_max-x)
+
+
+frag_mean = 250
+frag_sd = 70
+
+mrna1_len = 200
+mrna2_len = 300
+mrna3_len = 1000
+
+p1 = frag_factor_profile(mrna1_len, frag_mean, frag_sd)
+p2 = frag_factor_profile(mrna2_len, frag_mean, frag_sd)
+p3 = frag_factor_profile(mrna3_len, frag_mean, frag_sd)
+
+
+

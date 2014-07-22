da3555184f53ee99d51ebb5f1475ea4c386f4677
Thomas Gorochowski <tom@chofski.co.uk>
2014-07-22 18:17:59 -0400

    Attempt to combine flow and fragmentation models

---
+++ flow_model/flow_frag_model.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+"""
+Bring together the flow model output with the fragmentation profiles for
+fitting to the experimental data.
+"""
+
+__author__  = 'Thomas E. Gorochowski <tom@chofski.co.uk>, Voigt Lab, MIT'
+__license__ = 'OSI Non-Profit OSL 3.0'
+__version__ = '1.0'
+
+
+
+
+
+
+
+

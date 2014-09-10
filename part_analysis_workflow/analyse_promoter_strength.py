cb9b1cf2d764fe203e374434bfd02b5437d86684
Thomas Gorochowski <tom@chofski.co.uk>
2014-09-10 15:12:45 -0400

    New analysis workflow

---
+++ part_analysis_workflow/analyse_promoter_strength.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+"""Functions to aid in analysis of promoter strengths
+"""
+
+__author__  = 'Thomas E. Gorochowski <tom@chofski.co.uk>'
+__license__ = 'OSI Non-Profit OSL 3.0'
+__version__ = '1.0'
+
---
+++ part_analysis_workflow/analyse_sequence_context.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""Functions to aid in analysis of terminator strengths
+
+   1. Consider the termination efficiency (fractional termination) based
+      on the pre- and post-trace read depths.
+
+"""
+
+__author__  = 'Thomas E. Gorochowski <tom@chofski.co.uk>'
+__license__ = 'OSI Non-Profit OSL 3.0'
+__version__ = '1.0'
+
+
+
+

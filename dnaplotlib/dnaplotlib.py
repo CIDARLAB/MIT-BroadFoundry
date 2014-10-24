894274c0d233d27f7a5f2fb3f37abd903bb585f0
Thomas Gorochowski <tom@chofski.co.uk>
2014-10-24 09:44:25 -0400

    Rearrangement of library

--- dnaplotlib/dnaplotlib.py
+++ dnaplotlib/dnaplotlib.py
@@ -30,244 +30,6 @@ __author__  = 'Thomas E. Gorochowski <tom@chofski.co.uk>, Voigt Lab, MIT\n\
 __license__ = 'OSI Non-Profit OSL 3.0'
 __version__ = '1.0'
 
-class DNARenderer:
-
-	# Standard part types
-	STD_PART_TYPES = ['Promoter',
-	                  'CDS',
-                      'Terminator',
-                      'RBS',
-                      'Scar',
-                      'Spacer',
-                      'Ribozyme',
-                      'Ribonuclease',
-                      'ProteinStability',
-                      'Protease',
-                      'Operator',
-                      'Origin',
-                      'Insulator']
-
-	# Standard regulatory types
-	STD_REG_TYPES = ['Repression',
-	                 'Activation']
-
-	def __init__(self, y_scale=1.0, linewidth=1.0, 
-		         backbone_pad_left=0.0, backbone_pad_right=0.0):
-		"""Constructor to generate an empty DNARenderer.
-		"""
-		self.y_scale = y_scale
-		self.linewidth = linewidth
-		self.backbone_pad_left = backbone_pad_left
-		self.backbone_pad_right = backbone_pad_right
-		self.reg_height = 15
-
-	def renderDNA(self, ax, parts, part_renderers, regs=None, reg_renderers=None):
-		"""Render the parts on the DNA and regulation.
-			- parts_list: list of dicts defining the parts
-			- part_renderers: standard renderer functions dict to use
-			- regs: list of regulations on the DNA
-			- reg_renderers: dict of standard regulation renderers
-		"""
-
-		# Plot the parts to the axis
-		part_num = 0
-		prev_end = 0
-		first_start = 0
-		first_part = True
-		for part in parts:
-			keys = part.keys()
-			# Check the part has minimal details required
-			if 'type' in keys:
-				if 'fwd' not in keys:
-					part['fwd'] = 'True'
-				if 'start' not in keys:
-					if part['fwd'] == True:
-						part['start'] = part_num
-					else:
-						part['start'] = part_num+1
-				if 'end' not in keys:
-					if part['fwd'] == True:
-						part['end'] = part_num+1
-					else:
-						part['end'] = part_num
-				# Extract custom part options (if available)
-				part_opts = None
-				if 'opts' in part.keys():
-					part_opts = part['opts']
-				# Use the correct renderer
-				if 'renderer' in part.keys():
-					# Use custom renderer
-					prev_start, prev_end = part['renderer'](ax, part['type'], part_num, 
-						             part['start'], part['end'], prev_end,
-						             self.y_scale, self.linewidth, 
-						             opts=part_opts)
-
-					#update start,end for regulation
-					part['start'] = prev_start
-					part['end'] = prev_end
-
-					if first_part == True:
-						first_start = prev_start
-						first_part = False
-				else:
-					# Use standard renderer, if one exists
-					if part['type'] in part_renderers.keys():
-						prev_start, prev_end = part_renderers[part['type']](ax, 
-							           part['type'], part_num, 
-							           part['start'], part['end'], 
-							           prev_end, self.y_scale, 
-							           self.linewidth, opts=part_opts)
-						
-						#update start,end for regulation
-						part['start'] = prev_start
-						part['end'] = prev_end
-						
-						if first_part == True:
-							first_start = prev_start
-							first_part = False
-			part_num += 1
-		
-
-		# first pass to get all of the arcranges
-		if regs != None:
-
-			for reg in regs:
-				keys = reg.keys()
-
-				# Check the part has minimal details required
-				if 'type' in keys and 'from_part' in keys and 'to_part' in keys:
-					# Extract custom part options (if available)
-
-					reg_opts = None
-					if 'opts' in reg.keys():
-						reg_opts = reg['opts']
-					
-					if reg['type'] in reg_renderers.keys():
-						
-						##############################################################################
-						arcstart = (reg['from_part']['start'] + reg['from_part']['end']) / 2
-						arcend   = (reg['to_part']['start']   + reg['to_part']['end']) / 2
-						arcrange = [arcstart,arcend]
-						reg['arclength'] = fabs(arcstart-arcend)
-						reg['arc_height_index'] = 1
-						##############################################################################
-
-			#sort regs by arc ranges from shortest to longest
-			regs.sort(key=lambda x: x['arclength'], reverse=False)
-
-			reg_num = 0
-			
-
-			pos_arc_ranges = [] # arc above DNA backbone if to_part is fwd
-			neg_arc_ranges = [] # arc below DNA backbone if to_part is reverse
-			
-			current_max = 1
-
-			# second pass to render all the arcs
-			for reg in regs:
-				keys = reg.keys()
-
-				# Check the part has minimal details required
-				if 'type' in keys and 'from_part' in keys and 'to_part' in keys:
-					# Extract custom part options (if available)
-
-					reg_opts = None
-					if 'opts' in reg.keys():
-						reg_opts = reg['opts']
-					
-					if reg['type'] in reg_renderers.keys():
-						
-						##############################################################################
-						# arc height algorithm: greedy from left-to-right on DNA design
-						
-						arcstart = (reg['from_part']['start'] + reg['from_part']['end']) / 2
-						arcend   = (reg['to_part']['start']   + reg['to_part']['end']) / 2
-						
-						arcmin = min(arcstart,arcend)
-						arcmax = max(arcstart,arcend)
-						arcrange = [arcmin,arcmax,reg['arc_height_index']]
-						arc_height_index = 1
-						
-						# arc above if to_part is fwd
-						if(reg['to_part']['fwd'] == True):
-							# find max arc height index of ONLY the prior arcs that clash with the current arc
-							current_max = 1
-							for r in pos_arc_ranges:
-								if  (arcrange[0] > r[0] and arcrange[0] < r[1]):
-									if(r[2] > current_max):
-										current_max = r[2]
-								elif(arcrange[0] > r[1] and arcrange[0] < r[0]):
-									if(r[2] > current_max):
-										current_max = r[2]
-								elif(arcrange[1] > r[0] and arcrange[0] < r[1]):
-									if(r[2] > current_max):
-										current_max = r[2]
-								elif(arcrange[1] > r[1] and arcrange[0] < r[0]):
-									if(r[2] > current_max):
-										current_max = r[2]
-									
-							# if arcs cross over, increment the arc height index
-							for r in pos_arc_ranges:
-								if  (arcrange[0] > r[0] and arcrange[0] < r[1]):
-									reg['arc_height_index'] = current_max + 1
-									arcrange[2] = reg['arc_height_index']
-								elif(arcrange[0] > r[1] and arcrange[0] < r[0]):
-									reg['arc_height_index'] = current_max + 1
-									arcrange[2] = reg['arc_height_index']
-								elif(arcrange[1] > r[0] and arcrange[0] < r[1]):
-									reg['arc_height_index'] = current_max + 1
-									arcrange[2] = reg['arc_height_index']
-								elif(arcrange[1] > r[1] and arcrange[0] < r[0]):
-									reg['arc_height_index'] = current_max + 1
-									arcrange[2] = reg['arc_height_index']
-							pos_arc_ranges.append(arcrange)
-						
-						# arc below if to_part is reverse
-						else:
-							# find max arc height index
-							current_max = 1
-							for r in neg_arc_ranges:
-								if  (arcrange[0] > r[0] and arcrange[0] < r[1]):
-									if(r[2] > current_max):
-										current_max = r[2]
-								elif(arcrange[0] > r[1] and arcrange[0] < r[0]):
-									if(r[2] > current_max):
-										current_max = r[2]
-								elif(arcrange[1] > r[0] and arcrange[0] < r[1]):
-									if(r[2] > current_max):
-										current_max = r[2]
-								elif(arcrange[1] > r[1] and arcrange[0] < r[0]):
-									if(r[2] > current_max):
-										current_max = r[2]
-							
-							# if arcs cross over, increment the arc height index
-							for r in neg_arc_ranges:
-								if  (arcrange[0] > r[0] and arcrange[0] < r[1]):
-									reg['arc_height_index'] = current_max + 1
-									arcrange[2] = reg['arc_height_index']
-								elif(arcrange[0] > r[1] and arcrange[0] < r[0]):
-									reg['arc_height_index'] = current_max + 1
-									arcrange[2] = reg['arc_height_index']
-								elif(arcrange[1] > r[0] and arcrange[0] < r[1]):
-									reg['arc_height_index'] = current_max + 1
-									arcrange[2] = reg['arc_height_index']
-								elif(arcrange[1] > r[1] and arcrange[0] < r[0]):
-									reg['arc_height_index'] = current_max + 1
-									arcrange[2] = reg['arc_height_index']
-							neg_arc_ranges.append(arcrange)
-						##############################################################################
-						
-						reg_renderers[reg['type']](ax, reg['type'], 
-							           reg_num, reg['from_part'], 
-							           reg['to_part'], self.y_scale, 
-							           self.linewidth, reg['arc_height_index'], opts=reg_opts)
-				reg_num += 1
-		# Plot the backbone (z=1)
-		l1 = Line2D([first_start-self.backbone_pad_left,prev_end+self.backbone_pad_right],[0,0], 
-			        linewidth=self.linewidth, color=(0,0,0), zorder=10)
-		ax.add_line(l1)
-		return first_start, prev_end
-
 ###############################################################################
 # SBOL Compliant Icon Renderers
 ###############################################################################
@@ -1098,3 +860,267 @@ def regulation (ax, type, num, from_part, to_part, y_scale, linewidth, arc_heigh
 # TODO - Convert from GeneClusterLibrary visualisation package, and integrate dnaplotlib
 
 
+
+###############################################################################
+# The DNA renderer
+###############################################################################
+
+class DNARenderer:
+
+	# Standard part types
+	STD_PART_TYPES = ['Promoter',
+	                  'CDS',
+                      'Terminator',
+                      'RBS',
+                      'Scar',
+                      'Spacer',
+                      'Ribozyme',
+                      'Ribonuclease',
+                      'ProteinStability',
+                      'Protease',
+                      'Operator',
+                      'Origin',
+                      'Insulator']
+
+	# Standard regulatory types
+	STD_REG_TYPES = ['Repression',
+	                 'Activation']
+
+	def __init__(self, y_scale=1.0, linewidth=1.0, 
+		         backbone_pad_left=0.0, backbone_pad_right=0.0):
+		"""Constructor to generate an empty DNARenderer.
+		"""
+		self.y_scale = y_scale
+		self.linewidth = linewidth
+		self.backbone_pad_left = backbone_pad_left
+		self.backbone_pad_right = backbone_pad_right
+		self.reg_height = 15
+
+	def SBOL_part_renderers ():
+		return {
+			'Promoter'         :sbol_promoter, 
+			'CDS'              :sbol_cds, 
+			'Terminator'       :sbol_terminator,
+			'RBS'              :sbol_rbs,
+			'Scar'             :sbol_scar,
+			'Spacer'	       :sbol_spacer,
+			'Ribozyme'         :sbol_ribozyme,
+			'Ribonuclease'     :sbol_ribonuclease,
+			'ProteinStability' :sbol_protein_stability,
+			'Protease'         :sbol_protease,
+			'Operator'         :sbol_operator,
+			'Origin'           :sbol_origin,
+			'Insulator'        :sbol_insulator,
+			'Repressor'        :temporary_repressor} 
+
+	def std_reg_renderers ():
+		return {
+			'Repression' :repress, 
+			'Activation' :induce}
+
+	def renderDNA(self, ax, parts, part_renderers, regs=None, reg_renderers=None):
+		"""Render the parts on the DNA and regulation.
+			- parts_list: list of dicts defining the parts
+			- part_renderers: standard renderer functions dict to use
+			- regs: list of regulations on the DNA
+			- reg_renderers: dict of standard regulation renderers
+		"""
+
+		# Plot the parts to the axis
+		part_num = 0
+		prev_end = 0
+		first_start = 0
+		first_part = True
+		for part in parts:
+			keys = part.keys()
+			# Check the part has minimal details required
+			if 'type' in keys:
+				if 'fwd' not in keys:
+					part['fwd'] = 'True'
+				if 'start' not in keys:
+					if part['fwd'] == True:
+						part['start'] = part_num
+					else:
+						part['start'] = part_num+1
+				if 'end' not in keys:
+					if part['fwd'] == True:
+						part['end'] = part_num+1
+					else:
+						part['end'] = part_num
+				# Extract custom part options (if available)
+				part_opts = None
+				if 'opts' in part.keys():
+					part_opts = part['opts']
+				# Use the correct renderer
+				if 'renderer' in part.keys():
+					# Use custom renderer
+					prev_start, prev_end = part['renderer'](ax, part['type'], part_num, 
+						             part['start'], part['end'], prev_end,
+						             self.y_scale, self.linewidth, 
+						             opts=part_opts)
+
+					#update start,end for regulation
+					part['start'] = prev_start
+					part['end'] = prev_end
+
+					if first_part == True:
+						first_start = prev_start
+						first_part = False
+				else:
+					# Use standard renderer, if one exists
+					if part['type'] in part_renderers.keys():
+						prev_start, prev_end = part_renderers[part['type']](ax, 
+							           part['type'], part_num, 
+							           part['start'], part['end'], 
+							           prev_end, self.y_scale, 
+							           self.linewidth, opts=part_opts)
+						
+						#update start,end for regulation
+						part['start'] = prev_start
+						part['end'] = prev_end
+						
+						if first_part == True:
+							first_start = prev_start
+							first_part = False
+			part_num += 1
+		
+
+		# first pass to get all of the arcranges
+		if regs != None:
+
+			for reg in regs:
+				keys = reg.keys()
+
+				# Check the part has minimal details required
+				if 'type' in keys and 'from_part' in keys and 'to_part' in keys:
+					# Extract custom part options (if available)
+
+					reg_opts = None
+					if 'opts' in reg.keys():
+						reg_opts = reg['opts']
+					
+					if reg['type'] in reg_renderers.keys():
+						
+						##############################################################################
+						arcstart = (reg['from_part']['start'] + reg['from_part']['end']) / 2
+						arcend   = (reg['to_part']['start']   + reg['to_part']['end']) / 2
+						arcrange = [arcstart,arcend]
+						reg['arclength'] = fabs(arcstart-arcend)
+						reg['arc_height_index'] = 1
+						##############################################################################
+
+			#sort regs by arc ranges from shortest to longest
+			regs.sort(key=lambda x: x['arclength'], reverse=False)
+
+			reg_num = 0
+			
+
+			pos_arc_ranges = [] # arc above DNA backbone if to_part is fwd
+			neg_arc_ranges = [] # arc below DNA backbone if to_part is reverse
+			
+			current_max = 1
+
+			# second pass to render all the arcs
+			for reg in regs:
+				keys = reg.keys()
+
+				# Check the part has minimal details required
+				if 'type' in keys and 'from_part' in keys and 'to_part' in keys:
+					# Extract custom part options (if available)
+
+					reg_opts = None
+					if 'opts' in reg.keys():
+						reg_opts = reg['opts']
+					
+					if reg['type'] in reg_renderers.keys():
+						
+						##############################################################################
+						# arc height algorithm: greedy from left-to-right on DNA design
+						
+						arcstart = (reg['from_part']['start'] + reg['from_part']['end']) / 2
+						arcend   = (reg['to_part']['start']   + reg['to_part']['end']) / 2
+						
+						arcmin = min(arcstart,arcend)
+						arcmax = max(arcstart,arcend)
+						arcrange = [arcmin,arcmax,reg['arc_height_index']]
+						arc_height_index = 1
+						
+						# arc above if to_part is fwd
+						if(reg['to_part']['fwd'] == True):
+							# find max arc height index of ONLY the prior arcs that clash with the current arc
+							current_max = 1
+							for r in pos_arc_ranges:
+								if  (arcrange[0] > r[0] and arcrange[0] < r[1]):
+									if(r[2] > current_max):
+										current_max = r[2]
+								elif(arcrange[0] > r[1] and arcrange[0] < r[0]):
+									if(r[2] > current_max):
+										current_max = r[2]
+								elif(arcrange[1] > r[0] and arcrange[0] < r[1]):
+									if(r[2] > current_max):
+										current_max = r[2]
+								elif(arcrange[1] > r[1] and arcrange[0] < r[0]):
+									if(r[2] > current_max):
+										current_max = r[2]
+									
+							# if arcs cross over, increment the arc height index
+							for r in pos_arc_ranges:
+								if  (arcrange[0] > r[0] and arcrange[0] < r[1]):
+									reg['arc_height_index'] = current_max + 1
+									arcrange[2] = reg['arc_height_index']
+								elif(arcrange[0] > r[1] and arcrange[0] < r[0]):
+									reg['arc_height_index'] = current_max + 1
+									arcrange[2] = reg['arc_height_index']
+								elif(arcrange[1] > r[0] and arcrange[0] < r[1]):
+									reg['arc_height_index'] = current_max + 1
+									arcrange[2] = reg['arc_height_index']
+								elif(arcrange[1] > r[1] and arcrange[0] < r[0]):
+									reg['arc_height_index'] = current_max + 1
+									arcrange[2] = reg['arc_height_index']
+							pos_arc_ranges.append(arcrange)
+						
+						# arc below if to_part is reverse
+						else:
+							# find max arc height index
+							current_max = 1
+							for r in neg_arc_ranges:
+								if  (arcrange[0] > r[0] and arcrange[0] < r[1]):
+									if(r[2] > current_max):
+										current_max = r[2]
+								elif(arcrange[0] > r[1] and arcrange[0] < r[0]):
+									if(r[2] > current_max):
+										current_max = r[2]
+								elif(arcrange[1] > r[0] and arcrange[0] < r[1]):
+									if(r[2] > current_max):
+										current_max = r[2]
+								elif(arcrange[1] > r[1] and arcrange[0] < r[0]):
+									if(r[2] > current_max):
+										current_max = r[2]
+							
+							# if arcs cross over, increment the arc height index
+							for r in neg_arc_ranges:
+								if  (arcrange[0] > r[0] and arcrange[0] < r[1]):
+									reg['arc_height_index'] = current_max + 1
+									arcrange[2] = reg['arc_height_index']
+								elif(arcrange[0] > r[1] and arcrange[0] < r[0]):
+									reg['arc_height_index'] = current_max + 1
+									arcrange[2] = reg['arc_height_index']
+								elif(arcrange[1] > r[0] and arcrange[0] < r[1]):
+									reg['arc_height_index'] = current_max + 1
+									arcrange[2] = reg['arc_height_index']
+								elif(arcrange[1] > r[1] and arcrange[0] < r[0]):
+									reg['arc_height_index'] = current_max + 1
+									arcrange[2] = reg['arc_height_index']
+							neg_arc_ranges.append(arcrange)
+						##############################################################################
+						
+						reg_renderers[reg['type']](ax, reg['type'], 
+							           reg_num, reg['from_part'], 
+							           reg['to_part'], self.y_scale, 
+							           self.linewidth, reg['arc_height_index'], opts=reg_opts)
+				reg_num += 1
+		# Plot the backbone (z=1)
+		l1 = Line2D([first_start-self.backbone_pad_left,prev_end+self.backbone_pad_right],[0,0], 
+			        linewidth=self.linewidth, color=(0,0,0), zorder=10)
+		ax.add_line(l1)
+		return first_start, prev_end

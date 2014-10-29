063438c6a15438f9cf83c4deb32bb1bad6b115f0
Thomas Gorochowski <tom@chofski.co.uk>
2014-10-29 18:19:45 -0400

    Bug fixes

--- gene_cluster_library/test/test_tu_architecture.py
+++ gene_cluster_library/test/test_tu_architecture.py
@@ -11,7 +11,7 @@ nifs.load('./data/clean_nif_stata_library.txt')
 # Extract the TUs (test functions don't error)
 tus_basic = nifs.transcriptional_units(non_terminated=False, read_through=False)
 tus_all = nifs.transcriptional_units(read_through=True)
-print 'There are', len(tus_basic), 'normal TUs and', len(tus_all), 'full.'
+print 'There are', len(tus_basic), 'normal TUs and', len(tus_all), 'full. Should be (75 and 76).'
 
 # Extract all the sequences for the TUs (we can use this data for fitting)
 gca.generate_all_variant_tus_fasta (nifs, './tu_arch_seqs/tu_arch_')

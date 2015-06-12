c7bdb426abe9710eaeb0fcecc31df312e179ef55
azjl <alexzlim@mit.edu>
2015-06-12 17:19:12 -0400

    positive autoregulator comparison

--- dynamic_circuits/circuit test.py
+++ dynamic_circuits/circuit test.py
@@ -13,10 +13,11 @@ plt.ion()
 
 #Autoregulator
 xMax = 200.
-yMax = 60.
+yMax = 80.
 B = 1.                       # rate of production of Y
+B0 = 0.01                    # rate of auto-activation
 a = 0.02                     # rate of degradation/dilution
-X0 = [0,0]                   # initial concentration of Y
+X0 = [0,0,0]                   # initial concentration of Y
 Yst=B/a                      # steady state
 K = 50.                      # repression threshold
 Tr = math.log(2)/a           # response time
@@ -26,10 +27,12 @@ n = 4                        # hill coefficient
 def f(x, t):
         Xi = x[0]
         Yi = x[1]
+        Zi = x[2]
         # the model equations
         f0 = B/(1+math.pow(Xi/K,n)) - a*Xi
         f1 = B - a*Yi
-        return [f0,f1]
+        f2 = B/(1+math.pow(Xi/K,n)) + (B0-a)*Zi
+        return [f0,f1,f2]
         
 t = np.linspace(0, xMax, 1000)   # time grid
 
@@ -37,25 +40,32 @@ t = np.linspace(0, xMax, 1000)   # time grid
 soln = odeint(f, X0, t)
 X = soln[:,0]
 Y = soln[:,1]
+Z = soln[:,2]
 
 plt.figure()
 plt.axis([0, xMax, 0, yMax])
+plt.plot(t,Z, label = 'pos. autoregulator')
 plt.plot(t,Y, label = 'simple regulator')
 plt.plot(t,X, label = 'neg. autoregulator')
 plt.axhline(y=K/2, xmax=Tr/xMax, color='r', ls='dashed')
 plt.axvline(x=Tr, ymax=K/(2*yMax), color='r', ls='dashed')
-plt.title('Autoregulator')
+plt.title('Circuit Comparisons')
 plt.xlabel('Time')
-plt.ylabel('Concentration X')
+plt.ylabel('[Concentration]')
 plt.legend(loc=0)
 
 plt.figure()
 plt.axis([0,2,0,1])
-plt.axhline(y=0.5, xmax=0.5, color='r', ls='dashed')
-plt.axvline(x=0.68, ymax=0.5, color = 'r', ls='dashed')
+plt.axhline(y=0.5, xmax=0.625, color='r', ls='dashed')
+plt.axvline(x=0.675, ymax=0.5, color = 'r', ls='dashed')
 plt.axvline(x=1, ymax=0.5, color = 'r', ls='dashed')
+plt.axvline(x=1.25, ymax=0.5, color = 'r', ls='dashed')
+plt.plot(t/Tr,Z/Z[999], label = 'pos. autoregulator')
 plt.plot(t/Tr,Y/Yst, label = 'simple regulator')
 plt.plot(t/Tr,X/X[999], label = 'neg. autoregulator')
+plt.title('Circuit Comparisons')
+plt.xlabel('t/Tr')
+plt.ylabel('[X]/Xst')
 plt.legend(loc=0)
 
 

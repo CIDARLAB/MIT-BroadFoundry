767d5609c8a7e86ba4cd1d5bf467bed02243d192
azjl <alexzlim@mit.edu>
2015-06-17 17:51:11 -0400

    Dynamic Activator and Repressor

--- dynamic_circuits/dynamic_circuit_activator.py
+++ dynamic_circuits/dynamic_circuit_activator.py
@@ -10,38 +10,46 @@ import numpy as np
 from scipy.integrate import odeint
 plt.ion()
 
-xMax = 8
-yMax = 5.1 
+per = 4     # period of sinusoid 
 amp = 2     # amplitude of signal
-per = 4     # period of sinusoid
+nwaves = 2
+
+xMax = per*nwaves
+yMax = amp+0.1
+
 itr = 1000  # time iterations
-y = [0,0,0]     # initial conditions
+inic = [0,0,0]     # initial conditions
 Bx = 1     # rate of production of X
 ax = 0.2   # degradation rate of X
 By = 1     # rate of production of Y
 ay = 0.5      # degradation rate of Y
-n = 500     # hill coefficient
+Bz = 1     # rate of production of Z
+az = 0.5    # degradation rate of Z
+n = 4     # hill coefficient
 Ky = 1      # activation coefficient
        
 t = np.linspace(0, xMax, itr)   # time grid
 
 # solve the system dx/dt = f(x, t)
-def f(y, t):
-        Xi = y[0]
-        Yi = y[1]
-        Y2i = y[2]
-        # the model equations
+def f(inic, t):
+        Xi = inic[0]
+        Yi = inic[1]
+        Zi = inic[2]
+        # Input Signal Equation: wave function, ~.5*(amp-amp*cos(pi*t/(.5*per)))
         f0 = amp*np.pi/per*np.sin(np.pi*t/(0.5*per))
+        # Activator Equation
         f1 = By*(np.power(Xi,n)/(np.power(Xi,n)+np.power(Ky,n))) - ay*Yi
-        f2 = By/(1+np.power(Xi/Ky,n)) - ay*Y2i
+        # Repressor Equation        
+        f2 = Bz/(1+np.power(Xi/Ky,n)) - az*Zi
         return [f0 , f1, f2]
 
 # solve the DEs
-soln = odeint(f, y, t)
+soln = odeint(f, inic, t)
 X = soln[:,0]
 Y = soln[:,1]
-Y2 = soln[:,2]
+Z = soln[:,2]
 
+# Input Plot
 plt.figure()
 plt.axis([0,xMax,-0.1,yMax])
 plt.plot(t,X)
@@ -49,10 +57,13 @@ plt.xlabel('time')
 plt.ylabel('concentration')
 plt.title('Input: X concentration')
 
+# Plot of outputs: activity (activator and repressor)
 plt.figure()
-plt.plot(t,Y)
-plt.plot(t,Y2)
+plt.axis([0,xMax,0,2])
+plt.plot(t,Y, label = 'activator')
+plt.plot(t,Z, label = 'repressor')
 plt.xlabel('time')
 plt.ylabel('concentration')
-plt.title('Output: Y Activity')
+plt.title('Output: Activity')
+plt.legend(loc=0)
 

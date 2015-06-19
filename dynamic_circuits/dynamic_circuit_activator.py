517352edfdab96cde116524a123cbbc5ccfe4800
azjl <alexzlim@mit.edu>
2015-06-19 16:42:27 -0400

    Tidying up and Commenting

--- dynamic_circuits/dynamic_circuit_activator.py
+++ dynamic_circuits/dynamic_circuit_activator.py
@@ -11,29 +11,32 @@ import inputs
 from scipy.integrate import odeint
 plt.ion()
 
-xMax = 5
+xMax = 10
 yMax = 4
-itr = 100          # time iterations
-inic = [0,0]        # initial conditions
+itr = 500           # time iterations/resolution
+inic = [1,0]        # initial conditions
 By = 1.             # rate of production of Y
 ay = 0.5            # degradation rate of Y
 Bz = 1.             # rate of production of Z
 az = 0.5            # degradation rate of Z
-n = 1000.              # hill coefficient
-Ky = 1             # activation coefficient
+n = 2.              # hill coefficient
+K = 1.              # activation coefficient
+st = 2              # starting x-position
+dis = 1             # distance
+mag = 2             # magnitude
 
 t = np.linspace(0, xMax, itr)   # time grid
-X = inputs.stepInput(t, 3, 5)
+X = inputs.stepInput(t, st, dis, mag)
 
 # solve the system dx/dt = f(x, t)
 def f(y, t):
-        Xi = inputs.stepInput(t, 3, 5)
+        Xi = inputs.stepInput(t, st, dis, mag)
         Yi = y[0]
         Zi = y[1]    
         # Activator Equation
-        f0 = By*(np.power(Xi,n)/(np.power(Xi,n)+np.power(Ky,n))) - ay*Yi
+        f0 = By*(np.power(Xi,n)/(np.power(Xi,n)+np.power(K,n))) - ay*Yi
         # Repressor Equation        
-        f1 = Bz/(1+np.power(Xi/Ky,n)) - az*Zi
+        f1 = Bz/(1+np.power(Xi/K,n)) - az*Zi
         return [f0 , f1]
 
 # solve the DEs
@@ -41,21 +44,22 @@ soln = odeint(f, inic, t)
 Y = soln[:,0]
 Z = soln[:,1]
 
-# Input Plot
+# Input Plot: X-->Y and X--|Z
 plt.figure()
 plt.axis([0,xMax,0,yMax])
 plt.plot(t,X)
+plt.axhline(y=K, color='r', ls='--')
 plt.xlabel('time')
 plt.ylabel('concentration')
 plt.title('Input: X concentration')
 
-# Plot of outputs: activity (activator and repressor)
+# Plot of outputs: Hill Function Response activity (activator and repressor)
 plt.figure()
 plt.axis([0,xMax,0,2])
-#plt.plot(t,Y, label = 'activator')
-plt.plot(t,Z, label = 'repressor')
+plt.plot(t,Y, label = 'activator')
+plt.plot(t,Z, color='r', label = 'repressor')
 plt.xlabel('time')
 plt.ylabel('concentration')
-plt.title('Output: Activity')
+plt.title('Output: Hill Function Response')
 plt.legend(loc=0)
 
--- dynamic_circuits/inputs.py
+++ dynamic_circuits/inputs.py
@@ -3,38 +3,8 @@
 Created on Thu Jun 18 11:56:25 2015
 
 @author: Alex Lim
-Output of not gate IPTG vs activation
-Object, object
-activating promoter and gene
-simulate time scales
-production (B) and degradation (a) of generic proteins, rates (# protein/s)
-potential transcriptional (assume faster than translation) and translational steps
-Promoters models, attributes (inhibition function)
-Rbs = factor for the rate, (IPTG vs YFP)-->(t vs YFP @ IPTG = N)
-
-Information
-rate Repressor, steady state
-value of production rate (Rbs) and degradation rate
-Repressor (Pmin, Pmax, K, n)
-Gene (alpha(RBS),gamma
-design and derivative function for design, generic
-defining it
-Truth table and inputs
-
-Start with differential equations
-concentration (vector)
-
-promoter - not - promoter
-then nor
-output derivative calculates (only) change of each gene, given current state
-treat independently, observe changes
-
-long term goal:
-handle circuit with feedback
-data structure, dictionary, gene, states and how promoter activates it, where state comes from
-external or other gene input, parameter
-
 """
+
 import numpy as np
 from scipy import signal
 '''
@@ -42,7 +12,7 @@ Sinusoidal Input Function
 per = period of sinusoid 
 amp = amplitude of signal
 dis = angular displacement to the right
-bas = basal production rate
+bas = base of sinusoid wave
 '''
 def sinInput(t, per=4, amp=0.8, dis=0, bas=0):
     return bas + 0.5*amp*(1-np.cos(np.pi*(t-dis)/(0.5*per)))
@@ -57,38 +27,46 @@ def linInput(t, slp=0, intc=1):
     
 '''
 Single Step Input Function
-
+x0 = starting x-position of step signal
+dis = length of step signal
+step = magnitude of step signal
+bas = base of step signal
 '''
-def stepInput(t, lo, up):
-    loB = lo    # lower bound
-    upB = up    # upper bound
-    step = 2    # magnitude of signal
+def stepInput(t, x0=3, dis=2, step=2, bas=0):
     if isinstance(t,float):
-        if t >= loB and t <= upB:    
-            return step
+        if t >= x0 and t <= x0+dis:    
+            return bas+step
         else:
-            return 0
+            return bas
     else:       # For plotting an array x derived from array t
         output = [0]*t.size
         inc=0
         for i in t:
-            if i >= loB and i <= upB:
-                output[inc] = step
+            if i >= x0 and i <= x0+dis:
+                output[inc] = bas+step
                 inc+=1
             else:
-                output[inc] = 0
+                output[inc] = bas
                 inc+=1
         return output
 
-def squInput(t):
-    amp = 3
-    per = 2
-    return 0.5*amp*(1+signal.square(2*np.pi*t/per))        
-    
-def sawInput(t):
-    amp = 3
-    per = 2
-    return 0.5*amp*(1+signal.sawtooth(2*np.pi*t/per))
+'''
+Square Wave Input Function
+amp = Amplitude of square wave
+per = Period of square wave
+bas = base of wave
+'''
+def squInput(t, amp=3, per=2, bas=0):
+    return bas + 0.5*amp*(1+signal.square(2*np.pi*t/per))        
+
+'''
+Sawtooth Wave Input Function
+amp = Amplitude of wave
+per = Period of wave
+bas = base of wave
+'''    
+def sawInput(t, amp=3, per=2, bas=0):
+    return bas + 0.5*amp*(1+signal.sawtooth(2*np.pi*t/per))
     
     
     

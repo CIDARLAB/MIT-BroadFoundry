00de32b756576fd29eed4be145cf0b397b3e94fb
azjl <alexzlim@mit.edu>
2015-06-23 15:47:36 -0400

    Starting on Repressilators

---
+++ dynamic_circuits/arabidopsis_dynamic_repressilator_model.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 23 12:37:25 2015
+
+@author: Alex Lim
+"""
+
+'''
+STILL IN THE WORKINGS...
+reading paper
+code copied from repressilator_model.py
+'''
+
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.integrate import odeint
+plt.ion()
+
+xMin = 0
+xMax = 300
+yMin = 0
+yMax = 3
+itr = 1000          # time iterations
+inic = [2.1,0,0]      # initial conditions
+Bx = 2.             # rate of production of X
+ax = 0.1            # degradation rate of X
+Bx0 = 0             # basal production rate of X
+By = 2.             # rate of production of Y
+ay = 0.1            # degradation rate of Y
+By0 = 0             # basal production rate of Y
+Bz = 2              # rate of production of Z
+az = 0.1            # degradation rate of Z
+Bz0 = 0             # basal production rate of Z
+n = 2.5             # hill coefficient
+Kx = 0.5            # repression coefficient of X
+Ky = 0.5            # repression coefficient of Y
+Kz = 0.5            # repression coefficient of Z
+
+t = np.linspace(xMin, xMax, itr)   # time grid
+
+# solve the system dx/dt = f(x, t)
+def f(ic, t):
+        Xi = ic[0]
+        Yi = ic[1]    
+        Zi = ic[2]        
+        # Repressor Equations
+        f0 = Bx/(1+np.power(Zi/Kx,n)) - ax*Xi + Bx0
+        f1 = By/(1+np.power(Xi/Ky,n)) - ay*Yi + By0       
+        f2 = Bz/(1+np.power(Yi/Kz,n)) - az*Zi + Bz0
+        return [f0 , f1, f2]
+                
+# solve the DEs
+soln = odeint(f, inic, t)
+X = soln[:,0]
+Y = soln[:,1]
+Z = soln[:,2]
+
+plt.figure()
+plt.plot(t,X, label = 'X')
+plt.plot(t,Y, label = 'Y')
+plt.plot(t,Z, label = 'Z')
+plt.xlabel('time')
+plt.ylabel('concentration')
+plt.title('Output: Activity')
+plt.legend(loc=0)
---
+++ dynamic_circuits/repressilator_model.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 23 11:48:34 2015
+
+@author: Alex Lim
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.integrate import odeint
+plt.ion()
+
+xMin = 0
+xMax = 300
+yMin = 0
+yMax = 3
+itr = 1000          # time iterations
+inic = [0,1,0]      # initial conditions
+Bx = 1             # rate of production of X
+ax = 0.1            # degradation rate of X
+Bx0 = 0             # basal production rate of X
+By = 1             # rate of production of Y
+ay = 0.1            # degradation rate of Y
+By0 = 0             # basal production rate of Y
+Bz = 1              # rate of production of Z
+az = 0.1            # degradation rate of Z
+Bz0 = 0             # basal production rate of Z
+n = 2.5             # hill coefficient
+Kx = 0.5            # repression coefficient of X
+Ky = 0.5            # repression coefficient of Y
+Kz = 0.5            # repression coefficient of Z
+
+t = np.linspace(xMin, xMax, itr)   # time grid
+
+# solve the system dx/dt = f(x, t)
+def f(ic, t):
+        Xi = ic[0]
+        Yi = ic[1]    
+        Zi = ic[2]        
+        # Repressor Equations
+        f0 = Bx/(1+np.power(Zi/Kx,n)) - ax*Xi + Bx0
+        f1 = By/(1+np.power(Xi/Ky,n)) - ay*Yi + By0       
+        f2 = Bz/(1+np.power(Yi/Kz,n)) - az*Zi + Bz0
+        return [f0 , f1, f2]
+                
+# solve the DEs
+soln = odeint(f, inic, t)
+X = soln[:,0]
+Y = soln[:,1]
+Z = soln[:,2]
+
+plt.figure()
+plt.plot(t,X, label = 'X')
+plt.plot(t,Y, label = 'Y')
+plt.plot(t,Z, label = 'Z')
+plt.xlabel('time')
+plt.ylabel('concentration')
+plt.title('Output: Activity')
+plt.legend(loc=0)
+
+
+
+
+

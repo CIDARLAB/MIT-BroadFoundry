74df07d8746e22e9aca84f3ea37800a68a663e95
azjl <alexzlim@mit.edu>
2015-07-22 13:17:52 -0400

    Converging vale code

--- dynamic_circuits/Dynamic_Circuit_Prediction/General.py
+++ dynamic_circuits/Dynamic_Circuit_Prediction/General.py
@@ -12,6 +12,7 @@ import GeneralJsonIO
 from scipy.integrate import odeint
 import numpy as np
 
+#input is a json file
 def generateDynamicCircuitGraphs(fileName): 
 #---------------------------------------------------------------
     #Get all the values from the given file.
@@ -25,7 +26,7 @@ def generateDynamicCircuitGraphs(fileName):
     mRNAi = []
     proteini = []
     for i in range(len(logic_gate_names)):
-        #If a degredation constant alpha was not given, calculate it from the halflife
+        #If a degredation constant alpha was not given, calculate it from the half-life
         if input_and_logic_gate_dictionaries[input_and_logic_gate_names.index(logic_gate_names[i])]['am']=='NONE':
             input_and_logic_gate_dictionaries[input_and_logic_gate_names.index(logic_gate_names[i])]['am'] = np.log(2)/input_and_logic_gate_dictionaries[input_and_logic_gate_names.index(logic_gate_names[i])]['m_HALFLIFE']
         if input_and_logic_gate_dictionaries[input_and_logic_gate_names.index(logic_gate_names[i])]['ap']=='NONE':
@@ -34,10 +35,11 @@ def generateDynamicCircuitGraphs(fileName):
         #in logic_gate_names.
         mRNAi.append(input_and_logic_gate_dictionaries[input_and_logic_gate_names.index(logic_gate_names[i])]['Mi'])
         proteini.append(input_and_logic_gate_dictionaries[input_and_logic_gate_names.index(logic_gate_names[i])]['Pi'])
-    #combine into one vector of starting concentrations
+    #Combine into one vector of starting concentrations
     initc = mRNAi + proteini
     #Use odeint to calculate the concentrations over the time steps
     soln_f = odeint(f, initc, t, args=(input_and_logic_gate_dictionaries,input_and_logic_gate_names,logic_gate_names))
+    
     #Separate the mRNA and protein concentration vectors. The order they appear
     #in the list should be the same order that they are in in logic_gate_names.
     mRNA_f = []
@@ -46,6 +48,7 @@ def generateDynamicCircuitGraphs(fileName):
     for i in range(numGates):
         mRNA_f.append(soln_f[:,i])
         protein_f.append(soln_f[:,numGates+i])
+        
     #These are the possible graph lines/markers.
     color = ['b','g','r','c','m','y','k']
     linestyle = ['--','-','-.',':']
@@ -54,7 +57,19 @@ def generateDynamicCircuitGraphs(fileName):
     #Visualizing Inputs
     inputs_f = []
     inputNames = []
+
+#-------------------------------------------------------------------------------
+    numInputs = len(input_and_logic_gate_names)-len(logic_gate_names)
+    proteinListOfListOfValues = []
+    mRNAListOfListOfValues = []
+    for i in range(numGates):
+        proteinListOfListOfValues.append(getListOfConvergingValues(numInputs, time_axis_params['itr'], protein_f[i]))
+        mRNAListOfListOfValues.append(getListOfConvergingValues(numInputs, time_axis_params['itr'], mRNA_f[i]))    
     
+    print proteinListOfListOfValues[0]
+    print mRNAListOfListOfValues[0]
+#-------------------------------------------------------------------------------
+   
     for thing in input_and_logic_gate_names:
         #For each element in input_and_logic_gate_names, if it is not in logic_gate_names, it is an input.
         if thing not in logic_gate_names:
@@ -72,7 +87,8 @@ def generateDynamicCircuitGraphs(fileName):
             ymax = m
     plt.figure()
     plt.axis(xmin=time_axis_params['xMin'], xmax=time_axis_params['xMax'], ymax=ymax, ymin=-10)
-    #Plot each input against time with its name as the label
+   
+   #Plot each input against time with its name as the label
     for i in range(len(inputNames)):
         plt.plot(t,inputs_f[i],color=color[i%7],linestyle=linestyle[(i/7)%4],marker=marker[((i/7)/4)%17],label=inputNames[i])
     plt.xlabel('Time (min)')
@@ -80,6 +96,7 @@ def generateDynamicCircuitGraphs(fileName):
     plt.title('Concentration of Inputs Over Time')
     plt.legend(loc='center left', bbox_to_anchor=(1, 0.5))
     plt.show()
+    
     #Visualizing mRNA concentrations
     plt.figure()
     plt.axis(xmin=time_axis_params['xMin'], xmax=time_axis_params['xMax'])
@@ -125,3 +142,23 @@ def f(state, t, input_and_logic_gate_dictionaries, input_and_logic_gate_names, l
     #recombine into one list
     results = dinitmRNA_dt + dinitProtein_dt
     return results
+'''
+Alex was working here. Code for creating a list of the values that the graph 
+will converge to given for mRNA and proteins
+_______________________________________________________________________________
+'''
+    #List of truthvalues of gates
+    #tvs = input_and_logic_gate_dictionaries['TRUTHVALUES']    
+
+#Function for obtaining list of values given a graph of the output graph
+def getListOfConvergingValues(numInputs, numItr, opGraph):
+    output = []    
+    copy = opGraph
+    interval = numItr/2**numInputs
+    for i in range(2**numInputs):
+        output.append(copy[(i+1)*interval-2])
+    print output
+    return output
+'''
+_______________________________________________________________________________
+'''

f9f4077df3241dbba22b1c7ec38c8e57629b9f87
azjl <alexzlim@mit.edu>
2015-08-07 17:17:00 -0400

    XYSolver Coding

--- dynamic_circuits/DynamicCircuits/XYSolver/XYSolver.py
+++ dynamic_circuits/DynamicCircuits/XYSolver/XYSolver.py
@@ -11,56 +11,29 @@ import numpy as np
 import json
 from scipy.integrate import odeint
 plt.ion()
-'''
-def f(state, t):
-        X = state[0]
-        Y = state[1]
-        
-        for i in state:
-        # Repressor Protein Equations
-        
-        if(s == 'null'):
-            dX_dt = B0X + BX - aX*X # Nothing
-            dX_dt = B0X + BX/(1 + np.power(X/Km,n)) - aX*X # Auto-Repression
-            dX_dt = B0X + BX*np.power(X,n)/(np.power(X,n)+np.power(Km,n)) - aX*X # Auto-Activation
-            
-            dX_dt = B0X + BX - aX*X # Nothing
-            dX_dt = B0X + BX * np.power(Y,n)/(np.power(Y,n)+np.power(Km,n)) - aX*X # Activation
-            dX_dt = B0X + BX/(1 + np.power(Y/Km,n)) - aX*X # Repression
-            
-            
-            dY_dt = B0Y + BY - aY*Y
-            dY_dt = B0Y + BY*np.power(X,n)/(np.power(X,n)+np.power(Km,n)) - aY*Y
-            dY_dt = B0Y + BY/(1 + np.power(X/Km,n)) - aY*Y
-        elif():
-        return [dX_dt, dY_dt]
-'''
-def f(state, t, input_and_logic_gate_dictionaries, input_and_logic_gate_names, logic_gate_names):
+
+def f(state, t, parameter, vector):
+    #Set minimum value to 0.0 if negative.
     for i in range(len(state)):
         if state[i]<0 :
             state[i] = 0.0
             
-    #Concentration of mRNA and protein at this state
-    numGates = len(logic_gate_names)
-    #Split mRNA values from protein values
-    initmRNA = state[:numGates]
-    initProtein = state[numGates:]
+    #Concentration of proteins at this state
+    initProtein = state
     #Initialize the lists that hold the change in values (deltaValues).
-    dinitmRNA_dt = []
     dinitProtein_dt = []
 
     #For each gate we want to calculate the change in its mRNA and protein
-    for i in range(numGates):
+    for i in range(len(state)):
         #Retrieve some current Properties
-        gateProperties = input_and_logic_gate_dictionaries[input_and_logic_gate_names.index(logic_gate_names[i])]
-        initmRNAVal = initmRNA[i]
         initProteinVal = initProtein[i]
         #Get the changes and add it to the list holding the changes
-        dinitmRNA_dt.append(update.getmRNAChange(t,initmRNAVal,gateProperties,input_and_logic_gate_dictionaries,input_and_logic_gate_names,logic_gate_names,initProtein))
-        dinitProtein_dt.append(update.getProteinChange(initProteinVal,initmRNAVal,gateProperties))
-    #recombine into one list
-    results = dinitmRNA_dt + dinitProtein_dt
-    return results
+        if i == 0:
+            inp = 'X'
+        else:
+            inp = 'Y'
+        dinitProtein_dt.append(getProteinChange(initProtein,parameter[inp],vector))
+    return dinitProtein_dt
     
 def getFromJsonFile(fileLoc):
     '''
@@ -72,130 +45,77 @@ def getFromJsonFile(fileLoc):
     X_Y_vectors = data[1:]
     return X_Y_params, X_Y_vectors
 
-def getProteinChange(t, initProtein, parameter, vector):
-    if something == 'X':
-        param = parameter['X']
-        param2 = parameter['Y']
-    elif something == 'Y':
-        param = parameter['Y']
-        param2 = parameter['X']
+# initProtein is a dictionary, parameter is a dictionary of 2 dictionaries, 
+# vector is a single list of dictionaries of 'to's & 'from's
+def getProteinChange(initProtein, param, vector):
     # Degradation term and basal rate
     firstTerm = -param['a']*initProtein + param['B0']
-
+    name = param['name']
     B = param['B']
     Km = param['Km']
     n = param['n']
 
-    # Autoregulation term
-    secondTerm = 0
-    # Regulation term
-    thirdTerm = 0
-
+    # Regulation terms
+    addTerm = 0
     
-    #Repression
-    if vector['autoX'] == -1:
-        Input = initProtein['X']    #might not be instantiated yet.
-        secondTerm += repressor(B,Km,n,Input)
-    elif vector['autoX'] == 0:
-        secondTerm += 0
-    elif vector['autoX'] == 1:
-        secondTerm += activator(B,Km,n,Input)
-time_axis_params,input_and_logic_gate_dictionaries,input_and_logic_gate_names,logic_gate_names = GeneralJsonIO.getFromJsonFile(fileName)
-
-
-def getmRNAChange(t,initmRNAVal,gateProperties,input_and_logic_gate_dictionaries,input_and_logic_gate_names,logic_gate_names,initProtein):
-    #first term is degradation rate other terms are from
-    firstTerm = -gateProperties['am']*initmRNAVal
-    
-    #These gates only have one input so they only have two terms
-    if gateProperties['TYPE']=='BUFFER' or gateProperties['TYPE']=='NOT':
-        #Second term
-        mB = gateProperties['mB']
-        Km = gateProperties['Km']
-        n = gateProperties['n']
-        InputName = gateProperties['INPUT']
-        InputProperties = input_and_logic_gate_dictionaries[input_and_logic_gate_names.index(InputName)]
-        #If this is another gate then just get the concentration of the protein from it.
-        if InputProperties['TYPE'] != 'INPUT':
-            Input = initProtein[logic_gate_names.index(InputName)]
-        #If it is an input, then use the equation specified for the input to determine
-        #the concentration of the input at that time step.
-        else:
-            Inputfunc = InputProperties['INPUT']
-            Input = Inputfunc[0](t,*Inputfunc[1:])
-        if gateProperties['INPUT_EFFECT']=="REPRESS":
-            secondTerm = repressor(mB,Km,n,Input)
-        elif gateProperties['INPUT_EFFECT']=="ACTIVATE":
-            secondTerm = activator(mB,Km,n,Input)
-        answer =  firstTerm + secondTerm   
-        
-    #These gates have two inputs so they will have a total of three terms
-    elif gateProperties['TYPE']=='OR' or gateProperties['TYPE']=='NOR':
-        #Second term
-        mB1 = gateProperties['mB1']
-        Km1 = gateProperties['Km1']
-        n1 = gateProperties['n1']
-        InputName1 = gateProperties['INPUT1']
-        InputProperties1 = input_and_logic_gate_dictionaries[input_and_logic_gate_names.index(InputName1)]
-        #If this is another gate then just get the concentration of the protein from it.
-        if InputProperties1['TYPE'] != 'INPUT':
-            Input1 = initProtein[logic_gate_names.index(InputName1)]
-        #If it is an input, then use the equation specified for the input to determine
-        #the concentration of the input at that time step.
-        else:
-            Inputfunc1 = InputProperties1['INPUT']
-            Input1 = Inputfunc1[0](t,*Inputfunc1[1:])
-        if gateProperties['INPUT1_EFFECT']=="REPRESS":
-            secondTerm = repressor(mB1,Km1,n1,Input1)
-        elif gateProperties['INPUT1_EFFECT']=="ACTIVATE":
-            secondTerm = activator(mB1,Km1,n1,Input1)
-        #Third Term
-        mB2 = gateProperties['mB2']
-        Km2 = gateProperties['Km2']
-        n2 = gateProperties['n2']
-        InputName2 = gateProperties['INPUT2']
-        InputProperties2 = input_and_logic_gate_dictionaries[input_and_logic_gate_names.index(InputName2)]
-        #If this is another gate then just get the concentration of the protein from it.
-        if InputProperties2['TYPE'] != 'INPUT':
-            Input2 = initProtein[logic_gate_names.index(InputName2)]
-        #If it is an input, then use the equation specified for the input to determine
-        #the concentration of the input at that time step.
-        else:
-            Inputfunc2 = InputProperties2['INPUT']
-            Input2 = Inputfunc2[0](t,*Inputfunc2[1:])
-        if gateProperties['INPUT2_EFFECT']=="REPRESS":
-            thirdTerm = repressor(mB2,Km2,n2,Input2)
-        elif gateProperties['INPUT2_EFFECT']=="ACTIVATE":
-            thirdTerm = activator(mB2,Km2,n2,Input2)
-        answer = firstTerm + secondTerm + thirdTerm        
-        
+    # Activity of node X
+    if name == 'X':
+        for vec in vector:
+            if vec['to'] == 'X':
+                if vec['from'] == 'X':
+                    inputVal = initProtein
+                elif vec['from'] == 'Y':
+                    inputVal = initProtein
+                if vec['effect'] == -1:
+                    addTerm += repressor(B,Km,n,inputVal)
+                elif vec['effect'] == 1:
+                    addTerm += activator(B,Km,n,inputVal)
+                else:
+                    addTerm += 0
+        answer = firstTerm+addTerm
+    elif name == 'Y':
+        for vec in vector:
+            if vec['to'] == 'Y':
+                if vec['from'] == 'X':
+                    inputVal = initProtein['X']
+                elif vec['from'] == 'Y':
+                    inputVal = initProtein['Y']
+                if vec['effect'] == -1:
+                    addTerm += repressor(B,Km,n,inputVal)
+                elif vec['effect'] == 1:
+                    addTerm += activator(B,Km,n,inputVal)
+                else:
+                    addTerm += 0
+        answer = firstTerm+addTerm
     return answer
-    
 
 #Repressor and Activator Hill Equations
-def repressor(mB,Km,n,Input):
+def repressor(B,Km,n,Input):
     '''
-        Repressor equation
+        Repressor hill equation
     '''
     try:
-        return mB/(1+(Input/Km)**n)
+        return B/(1+(Input/Km)**n)
     #if Km is zero
     except ZeroDivisionError:
         try:
-            return mB*(Km**n)/(Input**n+Km**n)
+            return B*(Km**n)/(Input**n+Km**n)
         #if both Km and Input are zero
         except ZeroDivisionError:
             return 0
-def activator(mB,Km,n,Input):
+
+def activator(B,Km,n,Input):
     '''
-        Activator equation
+        Activator hill equation
     '''
     try:
-        return mB/(1+(Km/Input)**n)
+        return B/(1+(Km/Input)**n)
     #if Input is zero
     except ZeroDivisionError:
         try:
-            return mB*(Input**n)/(Input**n+Km**n)
+            return B*(Input**n)/(Input**n+Km**n)
         #if both Km and Input are zero
         except ZeroDivisionError:
             return 0
+
+
--- dynamic_circuits/DynamicCircuits/XYSolver/inputFileCreator.py
+++ dynamic_circuits/DynamicCircuits/XYSolver/inputFileCreator.py
@@ -24,34 +24,38 @@ aX = np.log(2)/hlX	# degradation rate
 B0X = 0.6 			# basal production rate
 BX = 600-B0X		# maximum production rate
 Xmax = (BX+B0X)/aX 	# maximum x production
+nX = 2 				# hill coefficient
+KmX = 40            # coefficient for activation or repression
 
 hlY = hlX
 aY = aX
 BY = BX
 B0Y = B0X
 Ymax = Xmax
+nY = 2
+KmY = 40
 
-n=2 				# hill coefficient
-Km=40				# coefficient for activation or repression
-
-inputD = {}
+inputD = {'X':{},'Y':{}}
+inputD['X']['name'] = 'X'
 inputD['X']['hl'] = hlX
 inputD['X']['a'] = aX
 inputD['X']['B0'] = B0X
 inputD['X']['B'] = BX
 inputD['X']['max'] = Xmax
-inputD['X']['n'] = n
-inputD['X']['Km'] = Km
+inputD['X']['n'] = nX
+inputD['X']['Km'] = KmX
 
+inputD['Y']['name'] = 'Y'
 inputD['Y']['hl'] = hlY
 inputD['Y']['a'] = aY
 inputD['Y']['B0'] = B0Y
 inputD['Y']['B'] = BY
 inputD['Y']['max'] = Ymax
-inputD['Y']['n'] = n
-inputD['Y']['Km'] = Km
+inputD['Y']['n'] = nY
+inputD['Y']['Km'] = KmY
 inputObject = [inputD]
 
+'''
 # values of -1 = repression, 0 = independent, 1 = activation for autoX/autoY/XtoY/YtoX
 # initial conditions set at 0, Xmax/2 and Xmax = B/a
 for autoX in range(-1,2):
@@ -69,12 +73,46 @@ for autoX in range(-1,2):
 						current['YtoX'] = YtoX
 						current['inic'] = [inicX,inicY]
 						inputObject.append(current)
-						'''
+						
+						
 						For individual files
 						fileName = 'input_'+str(autoX)+'_'+str(autoY)+'_'+str(XtoY)+'_'+str(YtoX)+'_'+str(iniX)+'_'+str(iniY)+'.json'
 						inputObject.writeToJson(fileName)
 						'''
+for XtoX in range(-1,1):
+	for XtoY in range(-1,1):
+		for YtoX in range(-1,1):
+			for YtoY in range(-1,1):
+				addOn = []
+				current = {}
+				current['to'] = 'X'
+				current['from'] = 'X'
+				current['effect'] = XtoX
+				addOn.append(current)
+
+				current = {}
+				current['to'] = 'X'
+				current['from'] = 'Y'
+				current['effect'] = XtoY
+				addOn.append(current)
+
+				current = {}
+				current['to'] = 'Y'
+				current['from'] = 'X'
+				current['effect'] = YtoX
+				addOn.append(current)
+
+				current = {}
+				current['to'] = 'Y'
+				current['from'] = 'Y'
+				current['effect'] = YtoY
+				addOn.append(current)
+
+				inputObject.append(addOn)
+
+
 #Creates the master input file
-fileName = 'masterXYInputFile.json'
+fileName = 'masterXYInputFile3.json'
 writeToJson(inputObject,fileName)
-	
+
+
